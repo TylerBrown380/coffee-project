e5bbfe2400304b2a5939399e6f8a698b5ea75813
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2020-08-20 14:15:14 -0600

    Initial commit

---
+++ CoffeeProject.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CoffeeProject.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ CoffeeProject.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CoffeeProject.Services
+{
+    public class Class1
+    {
+    }
+}
